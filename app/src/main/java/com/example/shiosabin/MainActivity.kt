16fc535dd8523d6617807cbfa171e2465d242d3b
MSI\nobuh <nobuhito110014@gmail.com>
2024-08-18 23:36:03 +0900

    first commit

---
+++ app/src/main/java/com/example/shiosabin/MainActivity.kt
@@ -0,0 +1,79 @@
+package com.example.shiosabin
+
+import android.os.Bundle
+import android.view.MenuItem
+import androidx.activity.OnBackPressedDispatcher
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.example.shiosabin.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
+    private lateinit var fragmentManager:FragmentManager
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //setSupportActionBar(binding.toolbar)
+        //SHIOSABINが表示されなくなる
+
+        val toggle = ActionBarDrawerToggle(this,binding.drawerLayout,binding.toolbar,R.string.nav_open,R.string.nav_close)
+        binding.drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        binding.navigationDrawer.setNavigationItemSelectedListener(this)
+
+        binding.bottomNavigation.background = null
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId)
+            {
+                R.id.bottom_sensor -> openFragment(SensorFragment())
+                R.id.bottom_map -> openFragment(MapFragment())
+                R.id.bottom_prediction -> openFragment(PredictionFragment())
+            }
+            true
+        }
+
+        fragmentManager = supportFragmentManager
+        openFragment(SensorFragment())
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when(item.itemId)
+        {
+            R.id.nav_notification -> openFragment(S_NotificationFragment())
+            R.id.nav_sharing -> openFragment(S_SharingFragment())
+            R.id.nav_gps -> openFragment(S_GPSFragment())
+        }
+        binding.drawerLayout.closeDrawer(GravityCompat.START)
+        return true
+    }
+
+    override fun onBackPressed() {
+        if(binding.drawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            binding.drawerLayout.closeDrawer(GravityCompat.START)
+        }
+        else
+        {
+            super.onBackPressed()
+        }
+    }
+
+    private fun openFragment(fragment:Fragment){
+        val fragmentTransaction:FragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment_container,fragment)
+        fragmentTransaction.commit()
+    }
+}
